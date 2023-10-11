a60e5591be28c6a1c92311ebc22149a3dc778f87
Lesly <leslymontufarj@gmail.com>
2023-10-11 03:50:40 -0300

    Corrigir win rate

--- js/adaline/main.js
+++ js/adaline/main.js
@@ -1,9 +1,9 @@
 class DataSet {
-    constructor(data, nro_in, nro_cases, 
-                target, nro_out) {
+    constructor({data, nro_in, 
+                target, nro_out}) {
         this.data = data;
         this.nro_in = nro_in;
-        this.nro_cases = nro_cases;
+        this.nro_cases = data.length/nro_in;
 
         this.target = target;
         this.nro_out = nro_out;
@@ -16,24 +16,35 @@ class DataSet {
     get x() {
         return this.data.slice(this.case*this.nro_in, this.case*this.nro_in+this.nro_in);
     }
-
-    // Method
-    // colect_x(case) {
-    //     return this.data.slice(case*nro_cases,case*(nro_cases+1)-1)
-    // }
 }
 
 class Adaline {
-    constructor(data, epochs, alpha = 0.1, theta = 0) {
+    constructor({data, epochs=1000, alpha = 0.001, theta = 0}) {
         this.alpha = alpha;
         this.theta = theta;
         this.data = data;
         this.epochs = epochs;
+        
+        this.tolerance = 1e-10;
+        this.dw = 0;
+        this.Biggerdw = 0;
         this.error = 0;
-        this.biggerw = 0;
+        this.globalError = 0;
 
-        this.w = this.randomUniform(this.data.nro_in*this.data.nro_out)
-        this.b = this.randomUniform(this.data.nro_out)
+        this.w = this.zeros(this.data.nro_in*this.data.nro_out);
+        this.b = this.zeros(this.data.nro_out);
+        this.y = [];
+
+        this.globalY = [];
+        this.winRate = 0;
+    }
+
+    zeros(n) {
+        const randomList = [];
+        for (let i = 0; i < n; i++) {
+            randomList.push(0.2);
+        }
+        return randomList;
     }
 
     randomUniform(n) {
@@ -47,51 +58,170 @@ class Adaline {
     }
 
     calculateY() {
-        c = 0
-        for(let j=0; i<this.data.nro_out; i++) { // colunm
-            for(let i=0; j<this.data.nro_in; j++) { // line a1xnro_in
+        let c = 0;
+        for(let j=0; j<this.data.nro_out; j++) { // colunm
+            for(let i=0; i<this.data.nro_in; i++) { // line a1xnro_in
                 c += this.data.x[i]*this.w[i*this.data.nro_out+j];
             }
-            this.y[j] = this.activationG(c)
+            this.y[j] = this.activationG(c);
         }
     }
 
     activationG(value) {
-        return (value>=0)*2-1
+        return ((value>=this.theta)*2-1)
     }
 
-    train(){
-        while(this.data.case<this.data.nro_cases) {
-            this.calculateY()
-            this.error += 1;
-            this.data.case++
+    updateWeights() {
+        this.error = 0;
+        let errYTarget;
+        for(let j=0; j<this.data.nro_out; j++) {
+            errYTarget = this.data.target[j]-this.y[j];
+            for(let i=0; i<this.data.nro_in; i++) {
+                this.dw = this.alpha*this.data.x[i]*errYTarget; // +a ou - 0, no caso de entrada e saida inteira
+                this.w[i*this.data.nro_out+j] += this.dw;
+                this.b[j] += this.alpha*errYTarget;                
+            }
+            this.error += errYTarget*errYTarget
+
+            this.dw = (this.dw > 0) ? this.dw : -this.dw;
+            if(this.dw>this.Biggerdw) {
+                this.Biggerdw = this.dw;
+            }
+            console.log(errYTarget)
+            if (errYTarget == 0){
+                this.winRate++;
+            }
         }
-        this.data.case = 0
-        return false
+        this.error /= (2*this.data.nro_out)
+    }
+
+    train(userWinRate = 100){
+        this.winRate = 0;
+        this.globalY = [];
+        this.globalError = 0;
+        while(this.data.case<this.data.nro_cases) {
+            this.calculateY();
+            this.updateWeights(); // calculando o erro global
+            this.globalError += this.error;
+            this.globalY = this.globalY.concat(this.y);
+            this.data.case++;
+        }
+        this.globalError /= this.data.nro_cases;
+        this.data.case = 0;
+        this.winRate = this.winRate /(this.data.nro_cases*this.data.nro_out)*100;
+        return (this.winRate < userWinRate) && (this.Biggerdw > this.tolerance);
     }
 }
 
 // inicio
-
-data = new DataSet(data = [-1, -1,
-                           -1, 1,
-                            1, -1,
-                            1, 1],
-                 nro_in = 2,
-                 nro_cases = 4,
-                 target = [-1, -1,
-                            1, 1,
-                            1, 1,
-                            1, 1],
-                 nro_out = 2)
-
-adaline = new Adaline(data, 1)
-
-epoch = 0
-continuar = true
-
-while (continuar && (epoch < adaline.epochs)) {
-    console.log(`Epoch: ${epoch}`)
-    continuar = adaline.train()
-    epoch++
+function log(message) {
+    const logContainer = document.getElementById('logContainer');
+    const logMessage = document.createElement('p');
+    logMessage.innerHTML = message;
+    logContainer.appendChild(logMessage);
 }
+
+const data = new DataSet({data: [-1, -1,
+                                -1, 1,
+                                1, -1,
+                                1, 1],
+                        nro_in: 2,
+                        target: [-1, -1,
+                                1, 1,
+                                1, 1,
+                                1, 1],
+                        nro_out: 2});
+
+let adaline = new Adaline({data, epochs:1});
+
+let epoch = 1;
+let continueCondition = true;
+
+while ((epoch <= adaline.epochs) && continueCondition) {
+    continueCondition = adaline.train(90);
+    log(`Epoch: ${epoch}<br>global error: ${adaline.globalError}<br>`)  
+    log(`w: ${adaline.w}<br>b: ${adaline.b}<br>`)
+    log(`y: ${adaline.globalY}<br>target: ${adaline.data.target}<br>`)
+    log(`winRate: ${adaline.winRate}%<br>`);
+    log(`continue: ${continueCondition}<br><br>`);
+    epoch++;
+}
+
+// message = `\\[
+//     \\begin{bmatrix}
+//       x_{11} & x_{12} & \\ldots & x_{1n} 
+//     \\end{bmatrix}
+//     \\cdot
+//     \\begin{bmatrix}
+//       w_{11} & w_{12} & \\ldots & w_{1p} \\\\
+//       w_{21} & w_{22} & \\ldots & w_{2p} \\\\
+//       \\vdots & \\vdots & \\ddots & \\vdots \\\\
+//       w_{n1} & w_{n2} & \\ldots & w_{np}
+//     \\end{bmatrix}
+//     +
+//     \\begin{bmatrix}
+//       b_{11} \\\\
+//       b_{21} \\\\
+//       \\ldots \\\\
+//       b_{n1} 
+//     \\end{bmatrix}
+//     =
+//     \\begin{bmatrix}
+//       y_{11} & y_{12} & \\ldots & y_{1p} 
+//     \\end{bmatrix}
+//     \\]`
+
+// eq = `<br>\\[
+//     \\begin{bmatrix}`
+
+// for (let i=1;i<data.nro_in;i++){
+//     eq += `x_{1${i}} & `
+// }
+
+// eq += `x_{1${data.nro_in}}
+//     \\end{bmatrix}
+//     \\cdot`
+
+// eq += `\\begin{bmatrix}`
+// for (let i=0; i<data.nro_in; i++){
+//     for (let j=0; j<data.nro_out-1; j++){
+//         eq += `${adaline.w[i*data.nro_out+j]} & `
+//     }
+//     eq += `${adaline.w[i*data.nro_out+data.nro_out-1]} \\\\`
+// }
+
+// eq += `\\end{bmatrix}
+// +`
+// eq += `\\begin{bmatrix}`
+
+// for (let i=0;i<data.nro_out-1;i++){
+//     eq += `${adaline.b[i]} \\\\`
+// }
+
+// eq += `${adaline.b[data.nro_out-1]}
+//     \\end{bmatrix}
+//     =`
+
+// eq += `\\begin{bmatrix}`
+
+// for (let i=1;i<data.nro_out;i++){
+//     eq += `y_{1${i}} & `
+// }
+
+// eq += `y_{1${data.nro_in}}
+//     \\end{bmatrix}`
+
+// eq += `\\approx
+// \\begin{bmatrix}`
+
+// for (let i=1;i<data.nro_out;i++){
+//     eq += `t_{1${i}} & `
+// }
+
+// eq += `t_{1${data.nro_in}}
+//     \\end{bmatrix}`
+
+// eq += `\\]`
+
+// log(message);
+// log(eq);
